a11cdb2ca5b3e3e1dc42617095fde48c87228208
qhrs <qpanda@126.com>
2019-09-28 18:49:15 +0800

    模板

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask
-import commands
+from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    printList = getPrints()
+
+    return render_template("test.html")
 
 def getPrints():
     '获取打印机列表'
-    (status, output) = commands.getstatusoutput("lpstat -p | grep printer |awk '{print $2}'")
-    printList = output.split('\n')
-    return printList
+    (status, output) = subprocess.getstatusoutput("lpstat -p | grep printer |awk '{print $2}'")
+    return output.split('\n')
 
 def getDefaultPrint():
     '获取默认打印机列表'
-    (status, output) = commands.getstatusoutput("lpstat -d | grep  'system default destination'| awk -F ':[ ]' '{print $2}'")
+    (status, output) = subprocess.getstatusoutput("lpstat -d | grep  'system default destination'| awk -F ':[ ]' '{print $2}'")
     return str(output)
 
 # https://gitlab.com/byygyy/ddns_ipv6/blob/master/ddns.py
@@ -27,8 +28,6 @@ def getDefaultPrint():
 # 显示作业 lpstat -W all
 # lpstat: Error - need "completed", "not-completed", or "all" after "-W" option.
 # 系统状态 lpstat -t  scheduler is running
-
-
 
 if __name__ == '__main__':
     app.run()
